3724f2ea65c81905bce30502336db20edf5e7d0f
stoshich <stoshich.stefan@yandex.ru>
2018-03-31 02:40:46 +0300

    sp7ex8

---
+++ С_П_7ex8.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+float a, b, resc=.0, resn=.0;
+int kolc, koln, e=1;
+
+printf("laalla\n");
+
+while (e != 0) {
+    scanf ("%i", &e);
+    if (e == 0){
+        printf("Кол-во четных: %i\n", kolc);
+        printf("Среднее значение четных: %f\n", resc / a);
+        printf("Кол-во нечетных: %i\n", koln);
+        printf("Среднее значение нечетных: %f\n", resn / b);
+    }
+    else if(e % 2 == 0) {
+        kolc++;
+        resc += e;
+        a = kolc;
+    }
+    else {
+        koln++;
+        b = koln;
+        resn += e;
+    }
+}
+return 0;
+}
